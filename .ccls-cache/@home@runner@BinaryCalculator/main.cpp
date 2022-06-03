a2eb2722c636633c75fdd3c6f80eb5ea7951f410
hotpig <93409262+MicahMaphet@users.noreply.github.com>
2022-06-03 03:07:45 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@BinaryCalculator/main.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+using namespace std;
+
+//These are the logic gates
+bool And(bool bool1, bool bool2) {
+  if (bool1 == 1) {
+  if (bool2 == 1) {
+  return 1; // code ends at return statement
+  }
+}
+  return 0;
+}
+
+bool Or(bool bool1, bool bool2) {
+  if (bool1 == 1) {
+  return 1;
+  }
+  if (bool2 == 1) {
+  return 1;
+  }
+  return 0;
+}
+bool Not(bool bool1) {
+  if (bool1 == 1) {
+  return 0;
+  }
+  return 1;
+}
+  
+void AddBinary(bool num1[4], bool num2[4]) {
+
+bool num3[4];  //the final output number
+
+/* the booleans below are just to store values to avoid running calculations over and over again */
+bool bitA;
+bool bitB;
+bool bitC;
+bool bitD;
+bool bitE;
+bool bitF;
+
+// Stage 1
+num3[3] = 
+And(
+Not(And(num1[3], num2[3])),
+Or(num1[3], num2[3])
+);
+
+/* Stage 1 finds the And value of the 4th digit of numbers
+1 and 2 and value being run through Not (inverted) and the
+4th digit of numbers 1 and 2 being run thorugh the Or. 
+All of the code is being run through And. Specificaly numbers
+1 and 2 4th number Not And, and numbers 1 and 2 4th number Or*/ 
+
+bitA = And(num1[3], num2[3]);
+/* This keeps track of a number that will be used on the 
+second section of Stage 2 */
+
+// Stage 2
+
+/*The value will be used twice, so it is better to declare 
+it in a seprate variable*/
+bitB = 
+And(
+Not(And(num1[2], num2[2])),
+Or(num1[2], num2[2]));
+
+num3[2] =
+And(
+Not(And(bitB, bitA)), //bitB is being used twice
+Or(bitB, bitA));
+
+bitC =// will be used later in Stage 3
+Or(
+And(num1[2], num2[2]),
+And(bitB, bitA));
+
+// Stage 3
+
+bitD = 
+And(
+Not(And(num1[1], num2[1])),
+Or(num1[1], num2[1]));
+/* The value of bitD will be used in two logic gate functions,
+so it is more effiecent to declare it in a variable in order to
+avoid repeating the same equartion twice*/
+
+num3[1] = 
+And(
+Not(And(bitD, bitC)), 
+Or(bitD, bitC));
+
+bitE =
+Or(
+And(num1[1], num2[1]),
+And(bitD, bitC));
+
+// Stage 4
+
+bitF =
+And(
+Not(And(num1[0], num2[0])),
+Or(num1[0], num2[0]));
+
+num3[0] =
+And(
+Not(And(bitF, bitE)),
+Or(bitF, bitE));
+
+cout << num1[0] << num1[1] << num1[2] << num1[3] << "\n"
+<< num2[0] << num2[1] << num2[2] << num2[3] << "\n"
+<< num3[0] << num3[1] << num3[2] << num3[3] ;
+
+/*cout << "\n"<< "\nbitA = " << bitA << "\nbitB = " <<bitB
+<< "\nbitC = " << bitC << "\nbitD = " << bitD << "\nbitE = "
+<< bitE << "\nbitF = " << bitF;*/ //extra data, mainly for testing
+}
+
+
+int main() {
+/* This is where you put the binary numbers. These are putting 
+the 4 bit values into an array for easier accesibility and 
+over all is easier to work with. */
+  bool num1[4] = {0, 0, 0, 1}; 
+  bool num2[4] = {0, 0, 1, 0};
+  cout << "type a 1 or 0\n";
+  cin >> num1[0];
+  cout << "type a 1 or 0\n";
+  cin >> num1[1];
+  cout << "type a 1 or 0\n";
+  cin >> num1[2];
+  cout << "type a 1 or 0\n";
+  cin >> num1[3];
+  
+  cout << "type a 1 or 0\n";
+  cin >> num2[0];
+  cout << "type a 1 or 0\n";
+  cin >> num2[1];
+  cout << "type a 1 or 0\n";
+  cin >> num2[2];
+  cout << "type a 1 or 0\n";
+  cin >> num2[3];
+
+  AddBinary(num1, num2); 
+/* This program cannot display numbers above 15
+If you add 1111 and 0001 it will display 0000 when the correct 
+ansewer is 10000*/
+  return 0;
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+This is a calculator that adds 4 digit binary numbers the way an actual computer does.
+Without, using the variable int, the operator +, just booleans.
+Except the main function int main.
+--Instructions-- 
+Either put 0 or 1, anything else will not work.
+The first 4 digits the program asks you to type
+are the first number.
+The last 4 are the second number.
+If you want to make sure it works, the first digit
+is the ones place, the second is the twos,
+the third one the fours place and the last one the
+eights place. It is in increaments of powers of 2.
+A computer calculates numbers in logic gates, for this very simple calculator it has 3 types of logic gates. 
+Their are other kinds of logic gates not listed.
+Not, 0 = 1, 1 = 0.
+When a number is passed through a Not logic gate, it inverts the value.
+And(1, 1) = 1. Both values have to be 1 to output 1, everything else is 0.
+Or(1, 0) = 1.
+Or is the opposite of And because to output 0, the input must be two 0s.
+The better way to look at it is, either of the values need to be 1 to output 1.
+I came up with all the code but not how it worked.
+I looked at how a computer adds numbers and then recreated it into C++.
+It is a model of how a computer adds basic numbers. 
+Since it is only meant to calculate 4 bit numbers, adding values that go over 15 will result in an incorrect ansewer. 
+The program stops calculating at the 4th digit.
+If you add 1111 (15) and 0001 (1), the result should be 10000 (16). This calculator will display 0000.
